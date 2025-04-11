40d9a733f7f9cfe11aa5758a9b27a8da24260f14
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-04-11 13:30:18 +0200

    Initial commit.

---
+++ src/test/java/pixel/academy/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
